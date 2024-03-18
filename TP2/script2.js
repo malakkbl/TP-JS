9adbdc34cb40b8c782e53fa78d42b959fc5a0964
unknown <malak012426@gmail.com>
2024-03-18 10:09:17 +0000

    Tp2- Exo3 done

--- TP2/script2.js
+++ TP2/script2.js
@@ -1,13 +1,25 @@
 function calculer() {
-    var num1 = parseInt(document.getElementById("number1_1").value);
-    var num2 = parseInt(document.getElementById("number1_2").value);
-    var result = num1 + num2;
-    document.getElementById("result1").textContent = result;
-  }
+  var num1 = parseInt(document.getElementById("number1_1").value);
+  var num2 = parseInt(document.getElementById("number1_2").value);
+  var result = num1 + num2;
+  document.getElementById("result1").textContent = result;
+}
 
-function permuter(){
-    var valeur1=document.getElementById("valeur2_1").value;
-    var valeur2=document.getElementById("valeur2_2").value;
-    document.getElementById("valeur2_1").value = valeur2;
-    document.getElementById("valeur2_2").value = valeur1;
+function permuter() {
+  var valeur1 = document.getElementById("valeur2_1").value;
+  var valeur2 = document.getElementById("valeur2_2").value;
+  document.getElementById("valeur2_1").value = valeur2;
+  document.getElementById("valeur2_2").value = valeur1;
+}
+
+function operation() {
+  var mon_operation = document.getElementById("operation3").value;
+
+  if (mon_operation === "") {
+    alert("Veuillez choisir une opération.");
+  } else {
+    var num1 = document.getElementById("number3_1").value;
+    var num2 = document.getElementById("number3_2").value;
+    document.getElementById("result3").textContent = eval(num1+mon_operation+num2);
+  }
 }

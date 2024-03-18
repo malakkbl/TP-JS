b0f956f790d77780b5851eef08eee09a794ada5f
unknown <malak012426@gmail.com>
2024-03-18 10:30:27 +0000

    Tp2- Exo4 et Exo5 done

--- TP2/script2.js
+++ TP2/script2.js
@@ -20,6 +20,75 @@ function operation() {
   } else {
     var num1 = document.getElementById("number3_1").value;
     var num2 = document.getElementById("number3_2").value;
-    document.getElementById("result3").textContent = eval(num1+mon_operation+num2);
+    document.getElementById("result3").textContent = eval(
+      num1 + mon_operation + num2
+    );
   }
+}
+
+function faireExo4() {
+  var saisie = prompt("Entrez trois nombres séparés par un espace:");
+  var nombres = saisie.split(" ");
+
+  if (
+    nombres.length !== 3 ||
+    isNaN(nombres[0]) ||
+    isNaN(nombres[1]) ||
+    isNaN(nombres[2])
+  ) {
+    document.getElementById("result4").innerHTML =
+      "Veuillez saisir trois nombres valides séparés par un espace.";
+    return;
+  }
+
+  var nombre1 = parseFloat(nombres[0]);
+  var nombre2 = parseFloat(nombres[1]);
+  var nombre3 = parseFloat(nombres[2]);
+
+  if (nombre1 === nombre2 && nombre2 === nombre3) {
+    document.getElementById("result4").innerHTML =
+      "Les trois variables sont identiques.";
+  } else if (
+    nombre1 === nombre2 ||
+    nombre1 === nombre3 ||
+    nombre2 === nombre3
+  ) {
+    document.getElementById("result4").innerHTML =
+      "Deux des variables sont de valeurs égales.";
+  } else {
+    document.getElementById("result4").innerHTML =
+      "Les trois variables sont différentes.";
+  }
+}
+
+function faireExo5() {
+  var prixHT = prompt("Entrez le prix HT :");
+
+  if (
+    prixHT === null ||
+    prixHT === "" ||
+    isNaN(prixHT) ||
+    parseFloat(prixHT) <= 0
+  ) {
+    document.body.innerText = "Prix HT invalide.";
+    return;
+  }
+
+  prixHT = parseFloat(prixHT);
+
+  var tauxTVA = prompt("Entrez le taux de TVA en pourcentage :");
+
+  if (
+    tauxTVA === null ||
+    tauxTVA === "" ||
+    isNaN(tauxTVA) ||
+    parseFloat(tauxTVA) <= 0
+  ) {
+    document.body.innerText = "Taux de TVA invalide.";
+    return;
+  }
+
+  var prixTTC = prixHT * (1 + parseFloat(tauxTVA) / 100);
+
+  document.body.innerText = "Le prix TTC est : " + prixTTC + " DHS";
 }
